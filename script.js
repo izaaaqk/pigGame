e0eac7f3b9f9deb9de89c33efb17b195e3a1eeef
Isaac Aguirre <isaac.aguirre@du.edu>
2022-07-09 10:52:39 -0600

    Refractor

--- script.js
+++ script.js
@@ -11,16 +11,28 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+let scores, currentScore, activePlayer, playing;
 
 // Starting conditions
-score0El.textContent ='0';
-score1El.textContent ='0';
-diceEl.classList.add('hidden');
+const init = function() {
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+    score0El.textContent ='0';
+    score1El.textContent ='0';
+    current0El.textContent = '0';
+    current1El.textContent = '0';
+
+    diceEl.classList.add('hidden');
+    player0El.classList.add('player--active');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--active');
+    player1El.classList.remove('player--winner');
+};
+
+init();
 
 const switchPlayer = function (){
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -78,19 +90,4 @@ btnHold.addEventListener('click', function() {
     }
 })
 
-btnNew.addEventListener('click', function() {
-    score0El.textContent ='0';
-    score1El.textContent ='0';
-    diceEl.classList.add('hidden');
-    scores[0] = 0;
-    scores[1] = 0;
-    currentScore = 0;
-    activePlayer = 0;
-    playing = true;
-    player0El.classList.add('player--active');
-    player0El.classList.remove('player--winner');
-    player1El.classList.remove('player--active');
-    player1El.classList.remove('player--winner');
-    current0El.textContent = '0';
-    current1El.textContent = '0';
-});
+btnNew.addEventListener('click', init);
